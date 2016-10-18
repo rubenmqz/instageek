38eabc0527add04222ca8c476046e75c0bef5d0a
Rubén Márquez <rubenmb@gmail.com>
2016-10-18 21:38:59 +0200

    Añadimos test de follow_api

--- followers/serializers.py
+++ followers/serializers.py
@@ -1,7 +1,23 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
 
+from followers.models import Relationship
+
+
 class RelationshipUserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
+
+
+class RelationshipSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Relationship
+        read_only_fields = ('origin',)
+
+    def validate_target(self, value):
+        return value
+
+    def validate(self, attrs):
+        return attrs
--- followers/views.py
+++ followers/views.py
@@ -1,8 +1,10 @@
-from rest_framework.mixins import ListModelMixin
+from rest_framework.mixins import ListModelMixin, CreateModelMixin
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import GenericViewSet
 from rest_framework.response import Response
-from followers.serializers import RelationshipUserSerializer
+
+from followers.models import Relationship
+from followers.serializers import RelationshipUserSerializer, RelationshipSerializer
 from followers.utils import get_following
 
 
@@ -13,3 +15,18 @@ class FollowingViewSet(ListModelMixin, GenericViewSet):
 
     def get_queryset(self):
         return get_following(self.request.user)
+
+
+class FollowViewSet(CreateModelMixin, GenericViewSet):
+
+    queryset = Relationship.objects.all()
+    serializer_class = RelationshipSerializer
+    permission_classes = (IsAuthenticated,)
+
+    def perform_create(self, serializer):
+        """
+        Hacemos que el usuario autenticado sea el seguidor
+        :param serializer:
+        :return:
+        """
+        serializer.save(origin=self.request.user)
