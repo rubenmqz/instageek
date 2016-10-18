42b809d3383183cc8e5364aca1b4ceea534a5726
Rubén Márquez <rubenmb@gmail.com>
2016-10-18 20:48:38 +0200

    Refactorizamos el viewset, y dividimos los tests en 2 archivos, en su propio paquete followers.tests

--- followers/tests/test_following_api_tests.py
+++ followers/tests/test_following_api_tests.py
@@ -1,39 +1,13 @@
 from django.contrib.auth.models import User
-from django.test import TestCase
 from django.test import override_settings
 from rest_framework import status
 from rest_framework.test import APITestCase
 
 from followers.models import Relationship
-from followers.utils import get_followers, get_following
 
-
-class RelationshipTests(TestCase):
-
-    def setUp(self):
-        """
-        Se ejecuta antes de cada test
-        :return:
-        """
-        self.user1 = User.objects.create_user("luke", "skywalker@starwars.com", "skywalker")
-        self.user2 = User.objects.create_user("anakin", "annie@starwars.com", "skywalker")
-
-        Relationship.objects.create(origin=self.user1, target=self.user2)  # user1 sigue a user2
-
-
-    def test_get_followers_returns_users_that_follow_a_given_user(self):
-        followers = get_followers(self.user2)
-        self.assertEqual([self.user1], followers) #followers == [user1]
-
-
-    def test_get_following_returns_users_that_a_given_user_follows(self):
-
-        following = get_following(self.user1)
-
-        self.assertEqual([self.user2], following)  # followers == [user1]
 
 @override_settings(ROOT_URLCONF='followers.urls')
-class APITests(APITestCase):
+class FollowingAPITests(APITestCase):
 
     FOLLOWING_API_URL = '/following/'
     USERS_PASSWORD = 'skywalker'
--- followers/views.py
+++ followers/views.py
@@ -1,3 +1,4 @@
+from rest_framework.mixins import ListModelMixin
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import GenericViewSet
 from rest_framework.response import Response
@@ -5,12 +6,10 @@ from followers.serializers import RelationshipUserSerializer
 from followers.utils import get_following
 
 
-class FollowingViewSet(GenericViewSet):
+class FollowingViewSet(ListModelMixin, GenericViewSet):
 
     serializer_class = RelationshipUserSerializer
     permission_classes = (IsAuthenticated,)
 
-    def list(self, request):
-        following = get_following(request.user)
-        serializer = RelationshipUserSerializer(following, many=True)
-        return Response(serializer.data)
+    def get_queryset(self):
+        return get_following(self.request.user)
